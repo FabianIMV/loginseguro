92b5c07c9fc6b3287cb62db0ccd291863576581f
FabianIMV <fabi.munozv@duocuc.cl>
2024-11-24 01:47:03 -0300

    Updates

---
+++ static/js/198.a1cd27c7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklogin_seguro_app=self.webpackChunklogin_seguro_app||[]).push([[198],{198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.a1cd27c7.chunk.js.map
